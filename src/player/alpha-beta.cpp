eb8867aa4fe3709993475b5a0e3224222176fd2a
ocg2012 <10112001terry@gmail.com>
2023-06-20 22:26:07 +0800

    31 commit

--- src/player/alpha-beta.cpp
+++ src/player/alpha-beta.cpp
@@ -43,7 +43,7 @@ void write_valid_spot(std::ofstream& fout) {
   //int depth=1;
   while(true) {
     // Choose a random spot.
-    auto move = AlphaBeta::get_move(root, 5);
+    auto move = AlphaBeta::get_move(root, 6);
     fout << move.first.first << " " << move.first.second << " "\
          << move.second.first << " " << move.second.second << std::endl;
     //depth+=1;
